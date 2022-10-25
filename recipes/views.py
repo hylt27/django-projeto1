95f7784c3d704e1e3886660912533d1bd11bfda2
Henrique Yukio <hylt27@gmail.com>
2022-10-25 18:55:28 -0300

    new changes

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Henrique'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Henrique'})
 
 
 # Create your views here.
