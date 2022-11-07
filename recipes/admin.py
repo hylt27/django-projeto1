58232edcd1f1ef3cb05c418439eb274e8a545fab
Henrique Yukio <hylt27@gmail.com>
2022-11-07 16:03:30 -0300

    updates

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
 # Register your models here.
+
+
+# uma classe para área administrativa de Category
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
