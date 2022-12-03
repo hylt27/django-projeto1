bfc199f2856d19a34825acf9457d0631d68d6749
Henrique Yukio <hylt27@gmail.com>
2022-12-03 12:51:35 -0300

    updates

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -36,3 +36,39 @@ class RecipeSearchViewTest(RecipeTestBase):
             'Search for &quot;test&quot;',
             response.content.decode('utf-8')
         )
+    
+    def test_search_by_title(self):
+        # tests if the search function can find
+        # a recipe by its title
+
+        title1 = 'Title recipe one'
+        title2 = 'Title recipe two'
+
+        recipe1 = self.make_recipe(
+            title=title1,
+            slug='recipe-one-slug',
+            author_data={
+                'username': 'username1'
+            }
+        )
+        recipe2 = self.make_recipe(
+            title=title2,
+            slug='recipe-two-slug',
+            author_data={
+                'username': 'username2'
+            }
+        )
+
+        url = reverse('recipes:search')
+        response1 = self.client.get(f'{url}?q={title1}')
+        response2 = self.client.get(f'{url}?q={title2}')
+        response_both = self.client.get(f'{url}?q=title') # the word 'title' is in both recipes
+
+        self.assertIn(recipe1, response1.context['recipes']) # recipe1 must be in response1
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes']) # recipe2 must be in response2
+        self.assertNotIn(recipe1, response2.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
